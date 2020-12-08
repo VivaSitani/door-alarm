b8610a2fbf784d2dd5cbd3c2fda850de186eacba
VivaSitani <66580922+VivaSitani@users.noreply.github.com>
2020-12-08 14:34:53 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,12 +1,9 @@
 input.onButtonPressed(Button.B, function () {
     basic.showIcon(IconNames.Angry)
-    music.setVolume(0)
 })
 basic.showIcon(IconNames.Happy)
-music.setVolume(127)
 basic.forever(function () {
     if (input.magneticForce(Dimension.Strength) <= 200) {
         basic.showIcon(IconNames.Angry)
-        music.playTone(988, music.beat(BeatFraction.Double))
     }
 })
