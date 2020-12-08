645ca08b226d37d9181d912e33750a3799cd3526
VivaSitani <66580922+VivaSitani@users.noreply.github.com>
2020-12-08 14:00:53 -0500

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,5 @@
 input.onButtonPressed(Button.A, function () {
-    basic.showNumber(input.magneticForce(Dimension.Strength))
+    basic.showIcon(IconNames.Happy)
 })
 basic.forever(function () {
 	if (input.magneticForce(Dimension.Strength) < 200) {
