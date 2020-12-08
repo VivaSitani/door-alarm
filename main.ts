8e21c0b478ced24ccf8cf250c3c455de0990265f
VivaSitani <66580922+VivaSitani@users.noreply.github.com>
2020-12-08 14:34:27 -0500

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,12 @@
-input.onButtonPressed(Button.A, function () {
-    basic.showIcon(IconNames.Happy)
+input.onButtonPressed(Button.B, function () {
+    basic.showIcon(IconNames.Angry)
+    music.setVolume(0)
 })
+basic.showIcon(IconNames.Happy)
+music.setVolume(127)
 basic.forever(function () {
-	if (input.magneticForce(Dimension.Strength) > 200) {
+    if (input.magneticForce(Dimension.Strength) <= 200) {
         basic.showIcon(IconNames.Angry)
+        music.playTone(988, music.beat(BeatFraction.Double))
     }
 })
